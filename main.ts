59eedcde7425c315bc3471c5401d593208442f3c
josiegmund22 <96065896+josiegmund22@users.noreply.github.com>
2021-12-13 08:57:51 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,6 +19,29 @@ function easy () {
         `, SpriteKind.Player)
     controller.moveSprite(easySprite)
     easySprite.setStayInScreen(true)
+    info.setLife(3)
+    info.setScore(0)
+    easyMeteor = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . c c c c . . 
+        . c c c c c . c c c c c f c c . 
+        c c a c c c c c 8 f f c f f c c 
+        c a f a a c c a f f c a a f f c 
+        c a 8 f a a c a c c c a a a a c 
+        c b c f a a a a a c c c c c c c 
+        c b b a a c f 8 a c c c 8 c c c 
+        . c b b a b c f a a a 8 8 c c . 
+        . . . . a a b b b a a 8 a c . . 
+        . . . . c b c a a c c b . . . . 
+        . . . . b b c c a b b a . . . . 
+        . . . . b b a b a 6 a . . . . . 
+        . . . . c b b b 6 6 c . . . . . 
+        . . . . . c a 6 6 b c . . . . . 
+        . . . . . . . c c c . . . . . . 
+        `, 50, 50)
+    while (info.life() > 0) {
+    	
+    }
     scene.setBackgroundImage(img`
         ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
         ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
@@ -141,44 +164,15 @@ function easy () {
         ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
         ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
         `)
-    info.setLife(3)
-    info.setScore(0)
-    easyMeteor = sprites.createProjectileFromSide(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . c c c c . . 
-        . c c c c c . c c c c c f c c . 
-        c c a c c c c c 8 f f c f f c c 
-        c a f a a c c a f f c a a f f c 
-        c a 8 f a a c a c c c a a a a c 
-        c b c f a a a a a c c c c c c c 
-        c b b a a c f 8 a c c c 8 c c c 
-        . c b b a b c f a a a 8 8 c c . 
-        . . . . a a b b b a a 8 a c . . 
-        . . . . c b c a a c c b . . . . 
-        . . . . b b c c a b b a . . . . 
-        . . . . b b a b a 6 a . . . . . 
-        . . . . c b b b 6 6 c . . . . . 
-        . . . . . c a 6 6 b c . . . . . 
-        . . . . . . . c c c . . . . . . 
-        `, 50, 50)
-    while (info.life() > 0) {
-        easyMeteor.setVelocity(50, 50)
-    }
 }
 function hard () {
 	
 }
-function easyMult (sec: number) {
-    EmultScore = sec * 1
-    return EmultScore
-}
-let EmultScore = 0
 let easyMeteor: Sprite = null
 let easySprite: Sprite = null
 let game2 = game.askForString("Select you difficulty. easy or hard?", 4)
 if (game2 == "easy") {
     easy()
-    easyMult(1)
 } else {
     hard()
 }
